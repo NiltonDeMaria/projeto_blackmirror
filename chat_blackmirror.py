43e9381bebefaa89db63c90bf6b97c4445bda1b5
Nilton Cesar da Costa Silva <niltondemariadev@gmail.com>
2024-03-28 08:58:10 -0300

    Envio do projeto de aula

---
+++ chat_blackmirror.py
@@ -0,0 +1,39 @@
+mensagem = input("Qual a sua dúvida? ")
+
+pergunta1 = "QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO?"
+
+pergunta2 = "QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO?"
+
+pergunta3 = "QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO?"
+
+pergunta4 = "COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA?"
+
+pergunta5 = "QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA?"
+
+pergunta6 = "QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?"
+
+pergunta7 = "O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACK MIRROR? EXPLIQUE."
+
+if mensagem == pergunta1:
+    print("Joan")
+
+elif mensagem == pergunta2:
+    print("Salma Hayek")
+
+elif mensagem == pergunta3:
+    print("Streamberry")
+
+elif mensagem == pergunta4:
+    print("Acessando a streaming.")
+
+elif mensagem == pergunta5:
+    print("Uma crise, tenta desligar a TV.")
+
+elif mensagem == pergunta6:
+    print("Privacidade na era digital. E políticas de privacidade.")
+
+elif mensagem == pergunta7:
+    print("Não sei nunca assistir para fazer essa relação.")
+
+else:
+    print("Perdão, não entendi sua pergunta. Faça novamente sua pergunta.")
